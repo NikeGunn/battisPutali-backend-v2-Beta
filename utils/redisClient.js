3fa04eb68e2816bf9ca20db3b3447414ca8698f0
Nikhil Bhagat <knewboy.nykhil@gmail.com>
2024-09-30 08:42:54 +0545

    implemented caching

---
+++ utils/redisClient.js
@@ -0,0 +1,13 @@
+// // utils/redisClient.js
+// import redis from 'redis';
+
+// const redisClient = redis.createClient();
+
+// redisClient.on('error', (err) => {
+//   console.log('Redis Client Error:', err);
+// });
+
+// redisClient.connect(); // Establish connection to Redis
+
+// export default redisClient;
+// // 
